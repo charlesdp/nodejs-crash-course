ccd9b54a60d7e6eb510b24af3d84567280af1581
Charles De Poortere <c.depoortere28@gmail.com>
2020-04-27 17:09:39 +0200

    createServer, req, res; PORT; Logger; events

--- index.js
+++ index.js
@@ -1,5 +1,93 @@
-const Person = require('./person.js');
+// const Person = require('./person.js');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-person1.greeting();
+// person1.greeting();
+
+////////////////////
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // if (req.url === '/') {
+  //   fs.readFile(
+  //     path.join(__dirname, 'public', 'index.html'),
+  //     (err, content) => {
+  //       if (err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html' });
+  //       res.end(content);
+  //     }
+  //   );
+  // }
+  // if (req.url === '/api/users') {
+  //   const users = [
+  //     { name: 'Arthur De Poortere', age: 28 },
+  //     { name: 'Alessandro Milly', age: 20 },
+  //     { name: 'Léo Milly', age: 17 }
+  //   ];
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify(users));
+  // }
+
+  // Build file path
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial content type
+  let contentType = 'text/html';
+
+  // Check ext and set content type
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'text/json';
+      break;
+    case '.png':
+      contentType = 'text/png';
+      break;
+    case '.jpg':
+      contentType = 'text/jpg';
+      break;
+  }
+
+  // Read file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, 'public', '404.html'),
+          (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text.html' });
+            res.end(content, 'utf8');
+          }
+        );
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      // Success
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
